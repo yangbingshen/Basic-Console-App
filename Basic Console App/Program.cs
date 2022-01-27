5b0a0d16678017616f7202e4a978eea652f686cb
yangb <yangb@LAPTOP-29N9KJRU>
2022-01-26 22:56:07 -0500

    Add project files.

---
+++ Basic Console App/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Basic_Console_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi my name is Bingshen Yang, i am doing the assignment 2");
+        }
+    }
+}
